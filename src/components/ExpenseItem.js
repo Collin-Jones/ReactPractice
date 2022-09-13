4c348edb93326ce4a2373d8a3a826b49d040e3f1
Collin Jones <collin.g.jones34@gmail.com>
2022-09-12 22:09:10 -0500

    moving the values to the app.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,6 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
-    const expenseDate = new Date(2022, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67
     return (
         <div className="expense-item">
             <div>{expenseDate.toISOString()}</div>
